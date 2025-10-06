c84843c8aaa7c1f3121680da7c424c80b63749eb
Yaxin Hu <1123844859@qq.com>
2025-10-06 18:16:38 +0800

    修改数据库连接bug

--- SpringBoot/src/main/java/org/example/springboot/mapper/EmployeeMapper.java
+++ SpringBoot/src/main/java/org/example/springboot/mapper/EmployeeMapper.java
@@ -1,10 +1,11 @@
 package org.example.springboot.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.example.springboot.entity.Employee;
 
 import java.util.List;
 
-
+@Mapper
 public interface EmployeeMapper {
     List<Employee> selectAll();
 }
